51c57096c8101ea13e51c296e4891ae84fc1c422
Robert Craig <rpcraig@tycho.ncsc.mil>
2013-01-24 14:49:25 -0500

    Adjustments to android property backend.
    Allow the android property backend parser to accept the
    SELABEL_OPT_VALIDATE option and to perform a validate
    callback.
    
    Change-Id: If061502c5e2489a1155798fac1d8357dbb8d13ba
    Signed-off-by: Robert Craig <rpcraig@tycho.ncsc.mil>

--- src/label_android_property.c
+++ src/label_android_property.c
@@ -126,6 +126,14 @@ static int process_line(struct selabel_handle *rec,
 				    path, lineno, context);
 		return -1;
 		}
+
+		if (rec->validating) {
+		        if (selabel_validate(rec, &spec_arr[nspec].lr) < 0) {
+			        selinux_log(SELINUX_WARNING,
+					    "%s:  line %d has invalid context %s\n",
+					    path, lineno, spec_arr[nspec].lr.ctx_raw);
+			}
+		}
      	}
 
 	data->nspec = ++nspec;
@@ -148,10 +156,6 @@ static int init(struct selabel_handle *rec, const struct selinux_opt *opts,
 		switch (opts[n].type) {
 		case SELABEL_OPT_PATH:
 			path = opts[n].value;
-			break;
-		default:
-			selinux_log(SELINUX_WARNING,
-				    "Argument type (%d) not recognized. Skipping\n", opts[n].type);
 			break;
 		}
 
