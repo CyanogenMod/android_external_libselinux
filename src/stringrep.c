MotorolaMobilityLLC/external-selinux

libselinux: Fix errors found by coverity
Signed-off-by: Eric Paris <eparis@redhat.com>
Acked-by: Dan Walsh <dwalsh@redhat.com>

libselinux/src/stringrep.c
@@ -258,17 +258,20 @@ static struct discover_class_node * discover_class(const char *s)
 		struct stat m;
 
 		snprintf(path, sizeof path, "%s/class/%s/perms/%s", selinux_mnt,s,dentry->d_name);
-		if (stat(path,&m) < 0)
+		fd = open(path, O_RDONLY | O_CLOEXEC);
+		if (fd < 0)
 			goto err4;
 
+		if (fstat(fd, &m) < 0) {
+			close(fd);
+			goto err4;
+		}
+
 		if (m.st_mode & S_IFDIR) {
+			close(fd);
 			dentry = readdir(dir);
 			continue;
 		}
-
-		fd = open(path, O_RDONLY);
-		if (fd < 0)
-			goto err4;
 
 		memset(buf, 0, sizeof(buf));
 		ret = read(fd, buf, sizeof(buf) - 1);
@@ -277,6 +280,9 @@ static struct discover_class_node * discover_class(const char *s)
 			goto err4;
 
 		if (sscanf(buf, "%u", &value) != 1)
+			goto err4;
+
+		if (value == 0 || value > NVECTORS)
 			goto err4;
 
 		node->perms[value-1] = strdup(dentry->d_name);

CyanogenMod/android_external_libselinux

commit aa62cd60f7192123b509c2518e7a2083e34a65a2 upstream.
Change-Id: I5e6222344b3baf4b9680aae1dad9652ce7d46f8a

src/stringrep.c
@@ -109,17 +109,20 @@ static struct discover_class_node * discover_class(const char *s)
 		struct stat m;
 
 		snprintf(path, sizeof path, "%s/class/%s/perms/%s", selinux_mnt,s,dentry->d_name);
-		if (stat(path,&m) < 0)
+		fd = open(path, O_RDONLY | O_CLOEXEC);
+		if (fd < 0)
 			goto err4;
 
+		if (fstat(fd, &m) < 0) {
+			close(fd);
+			goto err4;
+		}
+
 		if (m.st_mode & S_IFDIR) {
+			close(fd);
 			dentry = readdir(dir);
 			continue;
 		}
-
-		fd = open(path, O_RDONLY);
-		if (fd < 0)
-			goto err4;
 
 		memset(buf, 0, sizeof(buf));
 		ret = read(fd, buf, sizeof(buf) - 1);
@@ -128,6 +131,9 @@ static struct discover_class_node * discover_class(const char *s)
 			goto err4;
 
 		if (sscanf(buf, "%u", &value) != 1)
+			goto err4;
+
+		if (value == 0 || value > MAXVECTORS)
 			goto err4;
 
 		node->perms[value-1] = strdup(dentry->d_name);